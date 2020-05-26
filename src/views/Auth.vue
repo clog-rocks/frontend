fbf44ac3935a264a9d151da54244c6ddb2777342
cloggier <dev@clog.rocks>
2020-05-26 18:44:04 +0200

    fix: update Auth view
    - use updated getter
    - styling
    - make logo smaller on small screens (needs to be updated, most likely
      don't want to be centering vertically on small screens)

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -11,15 +11,17 @@
     </v-col>
     <v-col align-self="center" cols="12" md="4">
       <transition mode="out-in" name="component-fade">
-        <component :is="displayedComponent" />
+        <component :is="AUTH_COMPONENT" />
       </transition>
     </v-col>
   </v-row>
 </template>
 
 <script>
-import Login from "@/components/auth/Login.vue";
-import Register from "@/components/auth/Register.vue";
+import { mapActions, mapGetters } from "vuex";
+
+import Login from "@/components/auth/Login";
+import Register from "@/components/auth/Register";
 
 export default {
   name: "Auth",
@@ -27,10 +29,18 @@ export default {
     Login,
     Register,
   },
+
+  methods: {
+    ...mapActions(["dataNotRetrieved"]),
+  },
+
+  mounted() {
+    // Force data retrievel upon next login.
+    this.dataNotRetrieved();
+  },
+
   computed: {
-    displayedComponent() {
-      return this.$store.state.auth.activeComponent;
-    },
+    ...mapGetters(["AUTH_COMPONENT"]),
   },
 };
 </script>
@@ -38,6 +48,12 @@ export default {
 <style scoped>
 .logo {
   font-size: 8rem;
+}
+
+@media (max-width: 599.98px) {
+  .logo {
+    font-size: 6rem;
+  }
 }
 
 .component-fade-enter-active,
