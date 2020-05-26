5cbae0a9b66979c26a5c49d448afbfe22538ad89
Kamil Winczek <kamil@winczek.com>
2020-05-26 18:21:54 +0200

    feat: add core's and logbook's store and service

--- src/_services/auth.service.js
+++ src/_services/auth.service.js
@@ -2,56 +2,44 @@ import axios from "axios";
 
 export const authService = {
   // Login user.
-  login: (username, password) => {
+  login: async (username, password) => {
     const axiosOptions = {
       headers: { "Content-Type": "application/json" },
     };
 
     const body = JSON.stringify({ username, password });
 
-    return new Promise((resolve, reject) => {
-      axios
-        .post("http://127.0.0.1:8000/api/auth/login/", body, axiosOptions)
-        .then((response) => {
-          resolve(response.data);
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
+    return await axios.post(
+      "http://127.0.0.1:8000/api/auth/login/",
+      body,
+      axiosOptions
+    );
   },
 
   // Logout user.
-  logout: () => {
-    return new Promise((resolve, reject) => {
-      axios
-        .post("http://127.0.0.1:8000/api/auth/logout/", {})
-        .then((response) => {
-          resolve(response.data);
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
+  logout: async () => {
+    return await axios.post("http://127.0.0.1:8000/api/auth/logout/", {});
   },
 
   // Register new user.
-  register: (username, email, password) => {
+  register: async (username, email, password) => {
     const axiosOptions = {
       headers: { "Content-Type": "application/json" },
     };
 
     const body = JSON.stringify({ username, email, password });
 
-    return new Promise((resolve, reject) => {
-      axios
-        .post("http://127.0.0.1:8000/api/auth/register/", body, axiosOptions)
-        .then((response) => {
-          resolve(response.data);
-        })
-        .catch((error) => {
-          reject(error);
-        });
+    return await axios.post(
+      "http://127.0.0.1:8000/api/auth/register/",
+      body,
+      axiosOptions
+    );
+  },
+
+  // Check if user exists.
+  userExists: async (username) => {
+    return await axios.post("http://127.0.0.1:8000/api/auth/user/", {
+      username,
     });
   },
 };
--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,15 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import auth from "./modules/auth";
+import core from "./modules/core";
+import logbook from "./modules/logbook";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     auth,
+    core,
+    logbook,
   },
 });
