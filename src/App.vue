6d29a5d0e9d7d7c58ce461ddf5856c5929f4d57a
cloggier <dev@clog.rocks>
2020-05-26 18:40:17 +0200

    fix: fix detection of auth view
    This fixes detection of whether Auth components are active and
    adds/removes fill-height and fluid classed to/from v-container
    accordingly.
    
    Also adds fade transition.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <v-app>
-      <v-content>
-        <v-container :class="{ 'fill-height': isAuth }" fluid>
+  <v-app>
+    <v-content>
+      <v-container
+        :class="{ 'fill-height': isAuth, fluid: isAuth }"
+        class="pt-0"
+      >
+        <transition name="fade" appear mode="out-in">
           <router-view />
-        </v-container>
-      </v-content>
-    </v-app>
-  </div>
+        </transition>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 export default {
   name: "app",
+
   data: () => ({
-    isAuth: true,
+    isAuth: null,
   }),
 
+  mounted() {
+    this.isAuth = this.$router.currentRoute.name === "Auth";
+  },
+
   watch: {
+    // Center content of Auth components on screen.
+    // Timeout required to delay fill-height class change
+    // until transition finished.
     $route(to) {
-      // Center content of Auth components on screen.
-      console.log(to);
-      this.isAuth = to.name === "Auth";
+      setTimeout(() => {
+        this.isAuth = to.name == "Auth";
+      }, 500);
     },
   },
 };
 </script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400&display=swap");
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
