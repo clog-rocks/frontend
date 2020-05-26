398d18669f880571e4dc2a2762aac45b4bff9f43
Kamil Winczek <kamil@winczek.com>
2020-05-26 18:42:38 +0200

    feat: use axios.defaults for Authorization token

--- src/main.js
+++ src/main.js
@@ -4,7 +4,16 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 
+import axios from "axios";
+
 Vue.config.productionTip = false;
+
+// Set up global header for Axios if user is authenticated
+// when application starts.
+if (localStorage.getItem("token")) {
+  axios.defaults.headers.common["Authorization"] =
+    "Token " + localStorage.getItem("token");
+}
 
 new Vue({
   router,
