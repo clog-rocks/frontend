b1b59bea8cb011b806f6f9d1bdbac47e5f2e17f1
Kamil Winczek <kamil@winczek.com>
2020-05-26 18:46:36 +0200

    style: linter

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Auth from "../views/Auth.vue";
+import Auth from "@/views/Auth";
 import LogbookGlance from "@/views/LogbookGlance";
 
 Vue.use(VueRouter);
@@ -22,8 +22,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: () => import(/* webpackChunkName: "about" */ "@/views/About"),
   },
 ];
 
