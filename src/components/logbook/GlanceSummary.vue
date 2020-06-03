6d0190d0f2e9473e85c52a30ecd48ff373c176f3
cloggier <dev@clog.rocks>
2020-06-03 23:16:01 +0200

    refactor(logbook): move transition from component

--- src/components/logbook/GlanceSummary.vue
+++ src/components/logbook/GlanceSummary.vue
@@ -1,61 +1,59 @@
 <template>
-  <transition name="fade">
-    <div class="summary">
-      <h1>Hello {{ USER.username }},</h1>
-      <p class="lead my-3">
-        you have
-        <span class="enlarged">{{ ASCENT_COUNT }}</span> ascents and
-        <span class="enlarged">{{ CRAG_VISIT_COUNT }}</span> crag visits (<span
-          class="enlarged"
-          >{{ ASCENT_LAST_YEAR_COUNT }}</span
-        >
-        ascents and
-        <span class="enlarged">{{ CRAG_VISIT_LAST_YEAR_COUNT }}</span> crag
-        visits in last 12 months). Your most difficult route is
-        <span class="enlarged">8a</span>. You also onsighted
-        <span class="enlarged">7c+</span> and flashed
-        <span class="enlarged">7b+</span>. It has been
-        <span class="enlarged">7</span> years,
-        <span class="enlarged">1</span> month and
-        <span class="enlarged">4</span> days since you logged your first route.
-        You've climbed on average <span class="enlarged">255.29</span> routes a
-        year.
-      </p>
+  <div class="summary">
+    <h1>Hello {{ USER.username }},</h1>
+    <p class="lead my-3">
+      you have
+      <span class="enlarged">{{ ASCENT_COUNT }}</span> ascents and
+      <span class="enlarged">{{ CRAG_VISIT_COUNT }}</span> crag visits (<span
+        class="enlarged"
+        >{{ ASCENT_LAST_YEAR_COUNT }}</span
+      >
+      ascents and
+      <span class="enlarged">{{ CRAG_VISIT_LAST_YEAR_COUNT }}</span> crag visits
+      in last 12 months). Your most difficult route is
+      <span class="enlarged">8a</span>. You also onsighted
+      <span class="enlarged">7c+</span> and flashed
+      <span class="enlarged">7b+</span>. It has been
+      <span class="enlarged">7</span> years,
+      <span class="enlarged">1</span> month and
+      <span class="enlarged">4</span> days since you logged your first route.
+      You've climbed on average <span class="enlarged">255.29</span> routes a
+      year.
+    </p>
 
-      <div class="">
-        <table class="table-summary">
-          <thead>
-            <tr>
-              <th></th>
-              <th>Ascents</th>
-              <th>sessions</th>
-              <th>OS</th>
-              <th>FL</th>
-              <th>RP</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td class="category">all time</td>
-              <td class="enlarged-big">{{ ASCENT_COUNT }}</td>
-              <td class="enlarged-big">{{ CRAG_VISIT_COUNT }}</td>
-              <td class="enlarged-big">8a</td>
-              <td class="enlarged-big">7c</td>
-              <td class="enlarged-big">7b+</td>
-            </tr>
-            <tr>
-              <td class="category">last year</td>
-              <td class="enlarged">{{ ASCENT_LAST_YEAR_COUNT }}</td>
-              <td class="enlarged">{{ CRAG_VISIT_LAST_YEAR_COUNT }}</td>
-              <td class="enlarged">8a</td>
-              <td class="enlarged">7c</td>
-              <td class="enlarged">7b+</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+    <div class="">
+      <table class="table-summary">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Ascents</th>
+            <th>sessions</th>
+            <th>OS</th>
+            <th>FL</th>
+            <th>RP</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="category">all time</td>
+            <td class="enlarged-big">{{ ASCENT_COUNT }}</td>
+            <td class="enlarged-big">{{ CRAG_VISIT_COUNT }}</td>
+            <td class="enlarged-big">8a</td>
+            <td class="enlarged-big">7c</td>
+            <td class="enlarged-big">7b+</td>
+          </tr>
+          <tr>
+            <td class="category">last year</td>
+            <td class="enlarged">{{ ASCENT_LAST_YEAR_COUNT }}</td>
+            <td class="enlarged">{{ CRAG_VISIT_LAST_YEAR_COUNT }}</td>
+            <td class="enlarged">8a</td>
+            <td class="enlarged">7c</td>
+            <td class="enlarged">7b+</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  </transition>
+  </div>
 </template>
 
 <script>
