0767a4afb7c439fab3bd234b324e43fee74c7cc3
Kamil Winczek <kamil@winczek.com>
2020-06-04 16:15:54 +0200

    feat(router): add catch-all rule

--- src/router/index.js
+++ src/router/index.js
@@ -30,6 +30,10 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "@/views/About"),
   },
+  {
+    path: "/*",
+    redirect: { name: "LogbookGlance" },
+  },
 ];
 
 const router = new VueRouter({
