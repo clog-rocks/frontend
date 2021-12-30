5b5a20dc2dca77bd4a1079f486a67913afb9c59c
gegoune <69750637+gegoune@users.noreply.github.com>
2021-12-31 01:12:53 +0300

    chore: remove About view (#62)

--- src/router/index.js
+++ src/router/index.js
@@ -23,15 +23,6 @@ const routes = [
     component: TrainingView,
   },
   {
-    path: "/about",
-    name: "About",
-
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "@/views/About"),
-  },
-  {
     path: "/*",
     redirect: { name: "Logbook" },
   },
