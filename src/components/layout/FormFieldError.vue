37b70e0b52e7359eacade64b893c2ae8b7f3b384
gegoune <dev@clog.rocks>
2023-09-17 13:30:39 +0200

    feat: add simple FormInput and FormFieldError components

---
+++ src/components/layout/FormFieldError.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps<{
+  field: any;
+}>();
+</script>
+
+<template>
+  <div
+    v-for="error of props.field.$errors"
+    :key="error.$uid"
+  >
+    <small>{{ error.$message }}</small>
+  </div>
+</template>
+
+<style scoped>
+small {
+  padding-bottom: 0.25rem;
+  font-size: 0.75rem;
+  color: red;
+}
+</style>
---
+++ src/components/layout/FormInput.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import FormFieldError from "@/components/layout/FormFieldError.vue";
+
+const modelValue = defineModel<string>();
+const props = defineProps({
+  autocomplete: { type: String, required: true },
+  label: { type: String, required: true },
+  type: { type: String, default: "text" },
+  validator: { type: Object, required: true },
+});
+</script>
+
+<template>
+  <label>
+    {{ props.label }}
+    <input
+      v-model="modelValue"
+      :autocomplete="props.autocomplete"
+      :class="{ 'has-errors': props.validator.$error }"
+      :type="props.type"
+    />
+  </label>
+  <FormFieldError :field="validator" />
+</template>
+
+<style scoped>
+.has-errors {
+  border-color: red;
+}
+</style>
