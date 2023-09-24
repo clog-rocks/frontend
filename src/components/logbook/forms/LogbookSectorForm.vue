6e9595b64c7d83351050be39b72b5e938184ab15
gegoune <dev@clog.rocks>
2023-09-24 20:13:59 +0200

    feat: validate `LogbookSectorForm`

--- src/components/logbook/forms/LogbookSectorForm.vue
+++ src/components/logbook/forms/LogbookSectorForm.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { useVuelidate } from "@vuelidate/core";
+import { helpers, required } from "@vuelidate/validators";
+import { ref, unref } from "vue";
 import VueMultiselect from "vue-multiselect";
 
+import FormFieldError from "@/components/layout/FormFieldError.vue";
+import FormInput from "@/components/layout/FormInput.vue";
 import router from "@/router";
 import { useLogbookCragStore, useLogbookSectorStore } from "@/stores";
 import { type CragMultiselect, type SectorRequest } from "@/types/logbook";
@@ -15,40 +19,47 @@ const stores = {
   sector: useLogbookSectorStore(),
 };
 
-const name = ref<string>();
+const name = ref<string>("");
 const crag = ref<CragMultiselect | undefined>(
   props.cragId
     ? stores.crag.multiselect.find((crag) => crag.id == props.cragId)
     : undefined,
 );
 
-const submit = async function () {
-  // TODO: validate
-  if (!name.value || !crag.value) return;
-
-  const payload: SectorRequest = {
-    name: name.value,
-    crag: crag.value.id,
-  };
-
-  try {
-    const newSector = await stores.sector.create(payload);
-    router.push({
-      name: "logbook-route-new",
-      params: { sectorId: newSector.id },
-    });
-  } catch (error) {
-    console.log("Something went wrong submitting logbook/route.", error);
-  }
+const rules = {
+  crag: { required: helpers.withMessage("Crag is required", required) },
+  name: { required: helpers.withMessage("Sector is requried", required) },
 };
+
+const v$ = useVuelidate(rules, { crag, name });
+
+async function submit() {
+  const formValid = await unref(v$).$validate();
+  if (formValid && crag.value) {
+    const payload: SectorRequest = {
+      name: name.value,
+      crag: crag.value.id,
+    };
+
+    try {
+      const newSector = await stores.sector.create(payload);
+      router.push({
+        name: "logbook-route-new",
+        params: { sectorId: newSector.id },
+      });
+    } catch (error) {
+      console.log("Something went wrong submitting logbook/route.", error);
+    }
+  }
+}
 </script>
 
 <template>
   <form @submit.prevent>
     <h1>Add new sector</h1>
-    <label for="crag">Crag</label>
+    <label for="id_crag">Crag</label>
     <VueMultiselect
-      id="crag"
+      id="id_crag"
       v-model="crag"
       :options="stores.crag.multiselect"
       placeholder=""
@@ -58,25 +69,22 @@ const submit = async function () {
       track-by="id"
       :options-limit="20"
     >
-      <template #singleLabel="{ option }">
-        {{ option.country }} / <strong>{{ option.name }}</strong>
-      </template>
-      <template #option="{ option }">
+      <template
+        v-for="slotName in ['option', 'singleLabel']"
+        #[slotName]="{ option }"
+        :key="slotName"
+      >
         {{ option.country }} / <strong>{{ option.name }}</strong>
       </template>
     </VueMultiselect>
+    <FormFieldError :field="v$.crag" />
     <pre>{{ crag }}</pre>
     <RouterLink :to="{ name: 'logbook-crag-new' }">Add new crag</RouterLink>
-    <label>
-      Sector
-      <input
-        v-model="name"
-        type="text"
-        autocorrect="off"
-        spellcheck="false"
-        required
-      />
-    </label>
+    <FormInput
+      v-model="name"
+      label="Sector"
+      :validator="v$.name"
+    />
     <button @click="submit()">Add new sector</button>
   </form>
 </template>
