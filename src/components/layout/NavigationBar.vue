1538fda4b1dfd30614cfae50b2ec9e2ba40d4087
gegoune <dev@clog.rocks>
2023-10-02 10:40:44 +0200

    feat: NavigationBar only shows for authenticated views

--- src/components/layout/NavigationBar.vue
+++ src/components/layout/NavigationBar.vue
@@ -1,78 +1,40 @@
-<script setup lang="ts">
-import { useUserStore } from "@/stores";
-
-const userStore = useUserStore();
-</script>
-
 <template>
-  <header>
-    <p class="logo">clog</p>
-    <nav>
-      <TransitionGroup
-        tag="nav"
-        name="list"
+  <nav>
+    <div>
+      <RouterLink
+        key="logbook"
+        :to="{ name: 'logbook' }"
       >
-        <!-- Authenticated -->
-        <RouterLink
-          v-if="userStore.isAuthenticated"
-          key="logbook"
-          :to="{ name: 'logbook' }"
-        >
-          Logbook
-        </RouterLink>
-        <RouterLink
-          v-if="userStore.isAuthenticated"
-          key="training"
-          :to="{ name: 'training' }"
-        >
-          Training
-        </RouterLink>
-        <RouterLink
-          v-if="userStore.isAuthenticated && userStore.user?.username !== null"
-          key="user"
-          :to="{ name: 'user' }"
-        >
-          {{ userStore.user!.username }}
-        </RouterLink>
-        <RouterLink
-          v-if="userStore.isAuthenticated"
-          key="logout"
-          :to="{ name: 'logout' }"
-        >
-          Logout
-        </RouterLink>
-
-        <!-- Not authenticated -->
-        <RouterLink
-          v-if="!userStore.isAuthenticated"
-          key="login"
-          :to="{ name: 'login' }"
-        >
-          Login
-        </RouterLink>
-        <RouterLink
-          v-if="!userStore.isAuthenticated"
-          key="register"
-          :to="{ name: 'register' }"
-        >
-          Register
-        </RouterLink>
-      </TransitionGroup>
-    </nav>
-  </header>
+        Logbook
+      </RouterLink>
+      <RouterLink
+        key="training"
+        :to="{ name: 'training' }"
+      >
+        Training
+      </RouterLink>
+    </div>
+    <div>
+      <RouterLink
+        key="user"
+        :to="{ name: 'user' }"
+      >
+        Profile
+      </RouterLink>
+      <RouterLink
+        key="logout"
+        :to="{ name: 'logout' }"
+      >
+        Logout
+      </RouterLink>
+    </div>
+  </nav>
 </template>
 
 <style scoped>
-header {
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 nav {
+  display: flex;
+  justify-content: space-between;
   width: 100%;
   padding: 1rem 0 2rem;
 
