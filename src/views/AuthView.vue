3127d479b6c0f5c1a21bdf2f2246734be11e18b9
gegoune <69750637+gegoune@users.noreply.github.com>
2024-03-25 19:45:35 +0200

    style(forms): better centering of auth forms (#1063)

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,40 +1,31 @@
 <template>
-  <div class="center">
-    <router-view v-slot="{ Component }">
-      <transition
-        name="very-fast-fade"
-        mode="out-in"
-      >
-        <keep-alive>
-          <component :is="Component" />
-        </keep-alive>
-      </transition>
-    </router-view>
-  </div>
+  <router-view v-slot="{ Component }">
+    <transition
+      name="v"
+      mode="out-in"
+    >
+      <keep-alive>
+        <component :is="Component" />
+      </keep-alive>
+    </transition>
+  </router-view>
 </template>
-
-<style scoped>
-.center {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-}
-</style>
 
 <style>
 .auth-form {
-  @media (width >=600px) {
-    width: 500px;
-  }
-
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: min(100vw, 50ch);
   line-height: 2rem;
   background-color: transparent;
+  transform: translate(-50%, -50%);
 }
 
 .button-link-group {
   display: inline-flex;
   gap: 0.75rem;
   align-items: baseline;
+  margin-top: 1rem;
 }
 </style>
